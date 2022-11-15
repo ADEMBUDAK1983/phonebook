2fb3107c59d800b23e5fb34ece4b77ed8beec021
Adem Budak <87023856+ADEMBUDAK1983@users.noreply.github.com>
2022-11-15 22:29:43 +0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="xxxxxxxxxxxxxxx"
+TOKEN="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"
 cd /home/ec2-user && git clone https://$TOKEN@github.com/ADEMBUDAK1983/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
